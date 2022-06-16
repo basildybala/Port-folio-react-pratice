29c26b13cfb9c41d94422ee86225aff0923fe350
basildybala <akbasil123@gmail.com>
2022-06-16 08:33:50 +0530

    personal Stared

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./intro.css"
+
+const Intro = () => {
+  return (
+    <div className='i'>
+        <div className="i-left">
+            left
+        </div>
+        <div className="i-right">
+            Right
+        </div>
+    </div>
+  )
+}
+
+export default Intro
